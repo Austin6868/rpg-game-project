451959c0fad514b859b1a26d08e8dfe13859431c
jwhalls2 <59968453+jwhalls2@users.noreply.github.com>
2020-02-09 16:22:04 -0500

    Starting scripts for 2D player movement.

---
+++ Final Unity Project/Assets/_Scripts/Movement2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 10.0f;
+
+    void Update()
+    {
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+
+        transform.position += movement * Time.deltaTime * speed;
+
+    }
+}
