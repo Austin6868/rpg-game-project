c4457ae2325fd59ca8b32cccbca99741af0376f4
jwhalls2 <59968453+jwhalls2@users.noreply.github.com>
2020-02-09 17:10:47 -0500

    Player movement for the side scrolling portion is functional. Box colliders will have to be adjusted when we create our player model.

---
+++ Final Unity Project/Assets/_Scripts/Grounded.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+
+    GameObject Player;
+
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+        // Allows us to acces the player to check if it is grounded later.
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            Player.GetComponent<Movement2D>().isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<Movement2D>().isGrounded = false;
+
+        }
+    }
+}
--- Final Unity Project/Assets/_Scripts/Movement2D.cs
+++ Final Unity Project/Assets/_Scripts/Movement2D.cs
@@ -7,13 +7,28 @@ public class Movement2D : MonoBehaviour
     // Start is called before the first frame update
 
     public float speed = 10.0f;
+    public float jumpSpeed = 20.0f;
+    public bool isGrounded = false;
 
+   void Start()
+    {
+       
+    }
     void Update()
     {
+        Jump();
 
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
 
         transform.position += movement * Time.deltaTime * speed;
+    }
 
+    void Jump()
+    {
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
+        }
     }
 }
