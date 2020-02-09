b15ab6fcd11c2f1522fd155b54098422185baf67
jwhalls2 <59968453+jwhalls2@users.noreply.github.com>
2020-02-09 17:16:33 -0500

    Name Change and Camera Functionality

---
+++ Final Unity Project/Assets/_Scripts/CameraBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    public GameObject referance;
+    public Vector3 cameraOffset;
+
+    void Start()
+    {
+        cameraOffset = transform.position - referance.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = referance.transform.position + cameraOffset;
+    }
+}
