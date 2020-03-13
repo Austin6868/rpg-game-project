536e6f8a3bb684819268cd0371eb8056c3966a59
jwhalls2 <59968453+jwhalls2@users.noreply.github.com>
2020-03-12 21:47:49 -0400

    Character Model and Animations

---
+++ Final Epic Shooter Unity Project/Assets/_Script/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    Vector2 movement;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
