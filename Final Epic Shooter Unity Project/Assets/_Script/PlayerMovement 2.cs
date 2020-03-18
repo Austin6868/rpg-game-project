564347ba50fbf384d115306ff359c7b11b1a31db
Austin6868 <60564361+Austin6868@users.noreply.github.com>
2020-03-18 12:30:54 -0400

    Auto changes

---
+++ Final Epic Shooter Unity Project/Assets/_Script/PlayerMovement 2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    Vector2 movement;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if (movement.x == movement.y || movement.x == -movement.y)
+		{
+            movement.x = movement.x / Mathf.Sqrt(2f);
+            movement.y = movement.y / Mathf.Sqrt(2f);
+		}
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Final Epic Shooter Unity Project/Assets/_Script/Slot 2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class Slot : MonoBehaviour , IPointerClickHandler
+
+{
+    public int ID;
+    public string type;
+    public string description;
+    public bool empty;
+    public Texture2D icon;
+    // Start is called before the first frame update
+    public void OnPointerClick (PointerEventData pointerEventData)
+	{
+        useItem();
+        Debug.Log("clicked" + transform.name);
+	}
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void useItem()
+	{
+        
+        for (int i = 0; i < 10; i++)
+        {
+            if (transform.name == "Slot (" + (i+1) + ")")
+            {
+                Equipment.E.ModifySlot(i);//this is not the problem with the number i
+            }
+        }
+        
+    }
+}
