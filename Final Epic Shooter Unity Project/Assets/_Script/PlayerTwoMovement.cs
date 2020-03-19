e65dcecb85045a5a06c823ba7729a3a60305d989
Austin6868 <60564361+Austin6868@users.noreply.github.com>
2020-03-19 16:05:13 -0400

    Player2 able to teleport

--- Final Epic Shooter Unity Project/Assets/_Script/PlayerTwoMovement.cs
+++ Final Epic Shooter Unity Project/Assets/_Script/PlayerTwoMovement.cs
@@ -4,42 +4,38 @@ using UnityEngine;
 
 public class PlayerTwoMovement : PlayerMovement
 {
-
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public KeyCode[] combo;
+    public int currentIndex = 0;
 
     // Update is called once per frame
     
 	public override void FixedUpdate()
 	{
+
         float x = transform.position.x;
         float y = transform.position.y;
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
         
-            if(Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.Space))
+            if(movement.x == -1 && Input.GetKeyDown(KeyCode.Space))
 			{
             
                 transform.position = new Vector3(x - 1f, y, transform.position.z);
             
 			}
-			if(Input.GetKeyDown(KeyCode.D)&& Input.GetKeyDown(KeyCode.Space))
+			if(movement.x == 1 && Input.GetKeyDown(KeyCode.Space))
             {
             
                 transform.position = new Vector3(x + 1f, y, transform.position.z);
             
 
             }
-			if(Input.GetKeyDown(KeyCode.W)&& Input.GetKeyDown(KeyCode.Space))
+			if(movement.y == 1 && Input.GetKeyDown(KeyCode.Space))
 			{
             
                 transform.position = new Vector3(x, y + 1f, transform.position.z);
             
 			}
-			if (Input.GetKeyDown(KeyCode.S)&& Input.GetKeyDown(KeyCode.Space))
+			if (movement.y == -1 && Input.GetKeyDown(KeyCode.Space))
 			{
             
                 transform.position = new Vector3(x, y - 1f, transform.position.z);
