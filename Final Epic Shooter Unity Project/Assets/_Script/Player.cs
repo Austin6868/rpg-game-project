3ab185735f3933c2d64cf96603771a19033cfb20
Austin6868 <60564361+Austin6868@users.noreply.github.com>
2020-03-19 18:29:13 -0400

    Organized Project Assets

---
+++ Final Epic Shooter Unity Project/Assets/_Script/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
