d07957ed18ca6be8bb71c3a4461b49d66215c45d
jwhalls2 <59968453+jwhalls2@users.noreply.github.com>
2020-03-20 02:58:38 -0400

    GUI start menu + character instantiation

---
+++ Final Epic Shooter Unity Project/Assets/_Script/CharacterSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+
+    public void SelectCharacter()
+    {
+
+        if(gameObject.CompareTag("Hero 1"))
+        {
+            PlayerPrefs.SetInt("Hero", 1);
+
+            Debug.Log(PlayerPrefs.GetInt("Hero"));
+        }
+        else if(gameObject.CompareTag("Hero 2"))
+        {
+            PlayerPrefs.SetInt("Hero", 2);
+
+            Debug.Log(PlayerPrefs.GetInt("Hero"));
+        }
+    }
+}
---
+++ Final Epic Shooter Unity Project/Assets/_Script/InstPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstPlayer : MonoBehaviour
+{
+
+    public GameObject heroOne;
+    public GameObject heroTwo;
+
+    private void Start()
+    {
+        int hero = PlayerPrefs.GetInt("Hero");
+
+        if(hero == 1)
+        {
+            Instantiate(heroOne);
+        }
+        else if(hero == 2)
+        {
+            Instantiate(heroTwo);
+        }
+    }
+
+}
