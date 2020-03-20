e0fd33840b6291ac4defeec9f5a0e9dc93d326b8
Austin6868 <60564361+Austin6868@users.noreply.github.com>
2020-03-19 21:14:16 -0400

    commit before menu

--- Final Epic Shooter Unity Project/Assets/_Script/Player.cs
+++ Final Epic Shooter Unity Project/Assets/_Script/Player.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int currentHealth = 0;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+	public int maxHealth = 100;
+	public int currentHealth;
+
+	public HealthBar healthBar;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		currentHealth = maxHealth;
+		healthBar = FindObjectOfType<HealthBar>();
+		healthBar.SetMaxHealth(maxHealth);
         
-    }
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			TakeDamage(20);
+		}
+	}
+
+	void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+
+		healthBar.SetHealth(currentHealth);
+	}
 }
