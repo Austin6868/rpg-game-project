c5854a08c0eb6f5916b51d8f7aa6a8401abc8e80
Bardyy <battar@uwo.ca>
2020-03-19 22:06:42 -0400

    Enemy movement and shooting
    Enemy movement and shooting

---
+++ enemyMovementFinalEpicShooter/Assets/Scenes/_Scripts/EnemyMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    
+    public float speed;
+    public float stoppingDistance;
+    public float startTimeBtwShots;
+    public GameObject projectile;
+
+    private Transform player;
+    private float timeBtwShots;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+
+        if (timeBtwShots <= 0)
+        {
+
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+
+        }
+        else {
+
+            timeBtwShots -= Time.deltaTime;
+
+        }
+
+
+    }
+}
