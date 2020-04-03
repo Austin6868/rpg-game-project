69f8f28b008267adaf424963ef307a1030840e1a
Austin6868 <60564361+Austin6868@users.noreply.github.com>
2020-04-03 07:23:37 -0400

    minor changes

--- Final Epic Shooter Unity Project/Assets/__Scripts/displayExp.cs
+++ Final Epic Shooter Unity Project/Assets/__Scripts/displayExp.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class displayExp : MonoBehaviour
+public class DisplayExp : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
